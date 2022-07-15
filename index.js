637d3e505b1160d386a6f9d30fec637e0f094755
Marc Sànchez <marc.sanchez@launchmetrics.com>
2022-07-15 10:27:01 +0200

    feat: retrieving pr's

--- index.js
+++ index.js
@@ -3,23 +3,33 @@ const github = require("@actions/github");
 
 let githubClient;
 
-const getGithubClient = async (token) => {
+const getGithubClient = async () => {
   if (!githubClient) {
+    const token = core.getInput("token");
+    if (!token) throw new Error("You should provide a personal github token");
     githubClient = await github.getOctokit(token);
   }
   return getGithubClient;
 };
 
+const getOpenedPrs = async (baseBranch) => {
+  const client = await getGihubClient();
+  const prs = await client.request("GET /repos/{owner}/{repo}/pulls", {
+    owner: github.context.payload.repository.owner.name,
+    repo: github.context.payload.repository.name,
+    state: "open",
+    base: baseBranch,
+  });
+  return prs.data;
+};
+
 const exec = async () => {
-  const token = core.getInput("token");
-  if (!token) throw new Error("You should provide a personal github token");
-
-  //const client = await getGithubClient(token);
-  console.log(JSON.stringify(github.context));
   const baseBranch = core.getInput("baseBranch");
-
   if (!baseBranch)
     throw new Error("You are missing the base branch as parameter");
+
+  const prs = await getOpenedPrs();
+  console.log(prs);
 
   return "action finished";
 };
